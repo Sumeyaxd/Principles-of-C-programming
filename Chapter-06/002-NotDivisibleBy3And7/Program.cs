277193dcff7685450c3f05911f42b1d92e53eb87
sumeya <sumeya10@abv.bg>
2025-08-26 18:18:52 +0300

    Solution 2. exs

---
+++ Chapter-06/002-NotDivisibleBy3And7/Program.cs
@@ -0,0 +1,11 @@
+﻿int n = int.Parse(Console.ReadLine()!);
+
+for (int i = 1; i <= n; i++)
+{
+    // проверка: "не се делят едновременно на 3 и 7" 
+    // т.е. не делими на 21
+    if (i % 21 != 0)
+    {
+        Console.WriteLine(i);
+    }
+}
